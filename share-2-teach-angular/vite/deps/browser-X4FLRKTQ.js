c7c3a21b55961d61f9af92bccdb08420ce5925c0
hjaucamp <110494987+hjaucamp@users.noreply.github.com>
2024-10-16 23:47:49 +0200

    to run frontend
    m

---
+++ share-2-teach-angular/vite/deps/browser-X4FLRKTQ.js
@@ -0,0 +1,12 @@
+import {
+  __commonJS
+} from "./chunk-3OV72XIM.js";
+
+// node_modules/xhr2/lib/browser.js
+var require_browser = __commonJS({
+  "node_modules/xhr2/lib/browser.js"(exports, module) {
+    module.exports = XMLHttpRequest;
+  }
+});
+export default require_browser();
+//# sourceMappingURL=browser-X4FLRKTQ.js.map
---
+++ share-2-teach-angular/vite/deps_ssr/@angular_material_form-field.js
@@ -0,0 +1,45 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  MAT_ERROR,
+  MAT_FORM_FIELD,
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+  MAT_PREFIX,
+  MAT_SUFFIX,
+  MatError,
+  MatFormField,
+  MatFormFieldControl,
+  MatFormFieldModule,
+  MatHint,
+  MatLabel,
+  MatPrefix,
+  MatSuffix,
+  getMatFormFieldDuplicatedHintError,
+  getMatFormFieldMissingControlError,
+  getMatFormFieldPlaceholderConflictError,
+  matFormFieldAnimations
+} from "./chunk-LDX2RQCH.js";
+import "./chunk-6INRQAEJ.js";
+import "./chunk-ACDYJGWX.js";
+import "./chunk-EAFGYYQ3.js";
+import "./chunk-YDN5P6SV.js";
+import "./chunk-NQ4HTGF6.js";
+export {
+  MAT_ERROR,
+  MAT_FORM_FIELD,
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+  MAT_PREFIX,
+  MAT_SUFFIX,
+  MatError,
+  MatFormField,
+  MatFormFieldControl,
+  MatFormFieldModule,
+  MatHint,
+  MatLabel,
+  MatPrefix,
+  MatSuffix,
+  getMatFormFieldDuplicatedHintError,
+  getMatFormFieldMissingControlError,
+  getMatFormFieldPlaceholderConflictError,
+  matFormFieldAnimations
+};
+//# sourceMappingURL=@angular_material_form-field.js.map
